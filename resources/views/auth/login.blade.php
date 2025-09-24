4696aa9992d07b023b5f7a3442705cffea299653
revalinsyaa <160560652+revalinsyaa@users.noreply.github.com>
2025-09-24 09:08:16 +0700

    tampilan

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,110 +2,79 @@
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>SB Admin 2 - Login</title>
+    <title>Login</title>
 
-    <!-- Custom fonts for this template-->
-    <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <!-- Custom fonts -->
+    <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
 
-    <!-- Custom styles for this template-->
+    <!-- Custom styles -->
     <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
 
+    <style>
+        body {
+            background: url("assets/img/bg1.jpg") no-repeat center center fixed;
+            background-size: cover;
+        }
+
+        .card {
+            background: rgba(255, 255, 255, 0.9);
+            border-radius: 10px;
+        }
+    </style>
 </head>
 
-<body class="bg-gradient-primary">
+<body>
 
     <div class="container">
-
-        <!-- Outer Row -->
-        <div class="row justify-content-center">
-
-            <div class="col-xl-10 col-lg-12 col-md-9">
-
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
-                        <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                                    </div>
-                                    <form class="user" method="POST" action="{{ route('login') }}">
-                                        @csrf
-                                        <div class="form-group" for="email">
-                                            <x-input-label for="email" :value="__('Email')" />
-                                            <x-text-input id="email" class="form-control form-control-user"
-                                                type="email" name="email" :value="old('email')" required autofocus
-                                                autocomplete="username" placeholder="Enter Email Address..." />
-                                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                                        </div>
-                                        <div class="form-group" for="password">
-                                            <x-input-label for="password" :value="__('Password')" />
-
-                                            <x-text-input id="password" class="form-control form-control-user"
-                                                type="password" name="password" placeholder="Password" required
-                                                autocomplete="current-password" />
-
-                                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label>
-                                            </div>
-                                        </div>
-                                        <x-primary-button class="btn btn-primary btn-user btn-block">
-                                            {{ __('Log in') }}
-                                        </x-primary-button>
-                                        <hr>
-                                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                                            <i class="fab fa-google fa-fw"></i> Login with Google
-                                        </a>
-                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                        </a>
-                                    </form>
-                                    <hr>
-                                    <div class="text-center">
-                                        <a class="small" href="{{ route('password.request')}}">Forgot Password?</a>
-                                    </div>
-                                    <div class="text-center">
-                                        <a class="small" href="{{ route('register') }}">Create an Account!</a>
-                                    </div>
-                                </div>
-                            </div>
+        <div class="row justify-content-center align-items-center" style="min-height:100vh;">
+            <div class="col-lg-5">
+                <div class="card shadow-lg my-5">
+                    <div class="card-body p-4">
+                        <div class="text-center mb-4">
+                            <h1 class="h4 text-gray-900">Login</h1>
                         </div>
+
+                        <!-- Form Login -->
+                        <form method="POST" action="{{ route('login') }}">
+                            @csrf
+                            <div class="form-group">
+                                <x-input-label for="email" :value="__('Email')" />
+                                <x-text-input id="email" class="form-control"
+                                    type="email" name="email" :value="old('email')" required autofocus
+                                    placeholder="Enter Email Address" />
+                                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                            </div>
+
+                            <div class="form-group">
+                                <x-input-label for="password" :value="__('Password')" />
+                                <x-text-input id="password" class="form-control"
+                                    type="password" name="password" required
+                                    placeholder="Enter Password" />
+                                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                            </div>
+
+                            <x-primary-button class="btn btn-primary btn-block">
+                                {{ __('Log in') }}
+                            </x-primary-button>
+                        </form>
+                        <!-- End Form -->
                     </div>
                 </div>
-
             </div>
 
         </div>
-
     </div>
 
-    <!-- Bootstrap core JavaScript-->
+    <!-- Bootstrap core JavaScript -->
     <script src="assets/vendor/jquery/jquery.min.js"></script>
     <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Core plugin JavaScript-->
-    <script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
-
-    <!-- Custom scripts for all pages-->
+    <!-- Custom scripts -->
     <script src="assets/js/sb-admin-2.min.js"></script>
-
 </body>
 
 </html>
